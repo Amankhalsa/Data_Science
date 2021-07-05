9f55db75934181c78ccea94331d9ff42de751207
Amankhalsa <66160544+Amankhalsa@users.noreply.github.com>
2021-07-05 20:05:20 +0530

    Add files via upload

---
+++ part8.py
@@ -0,0 +1,20 @@
+
+import matplotlib.pyplot as plt
+# sales = [1, 2, 3, 4, 5]
+# budget = [3, 2, 1, 2, 5]
+# plt.title("Google add ")
+# plt.xlabel(" X Axis ")
+# plt.ylabel(" Y Axis")
+# plt.plot(sales, budget)
+# plt.show()
+m=1
+c=2
+x=[1,2,3,4,5]
+y=[]
+for t in x:
+    e=m*t+c
+    y.append(e)
+print(y)
+print(x)
+
+
